35447c99ab9ee5cd9e053c42a9264f43fde55191
Cristiano Dalbem <ninja.dalbem@gmail.com>
2019-07-08 04:20:52 -0300

    Updates

---
+++ precache-manifest.80f91b9a8ab7f4e0aa09ad3af9c927e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7e8c80a901aacac6dfd35cbbd019d3",
+    "url": "/openbikemap/index.html"
+  },
+  {
+    "revision": "4813c7847166ce087022",
+    "url": "/openbikemap/static/css/2.588d0d70.chunk.css"
+  },
+  {
+    "revision": "780a19dee919093db68f",
+    "url": "/openbikemap/static/css/main.82be95bb.chunk.css"
+  },
+  {
+    "revision": "4813c7847166ce087022",
+    "url": "/openbikemap/static/js/2.fe5616f9.chunk.js"
+  },
+  {
+    "revision": "780a19dee919093db68f",
+    "url": "/openbikemap/static/js/main.41407020.chunk.js"
+  },
+  {
+    "revision": "2e788ac644fce2e18bec",
+    "url": "/openbikemap/static/js/runtime~main.e616c564.js"
+  }
+]);
